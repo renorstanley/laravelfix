9f68af3b4acd9bc8fc99317bef8dda177966792c
RHeartz <rihito_tsubasa@yahoo.com>
2016-03-15 00:50:06 +0700

    Update crudcontroller.php

--- privasi/app/Http/Controllers/crudcontroller.php
+++ privasi/app/Http/Controllers/crudcontroller.php
@@ -20,12 +20,33 @@ class crudcontroller extends Controller
 
  		DB::table('siswa')->insert($data);
  	
-	return Redirect::to('/read');//->with('message,'berhasil menambah data');
+		return Redirect::to('/read')->with('message','berhasil menambah data');
  	}
 	
  	public function lihatdata(){
+ 		//Coding lama
+ 		//$data = DB::table('siswa')->get();
+ 		//return View::make('read')->with('siswa',$data);
+ 		
+ 		//Cara 1, lebih dianjurkan karena compact memudahkan hidup programmer, ga usah bingung mikir array [key => value]
+ 		//Compact berfungsi untuk membuat array dengan ketentuan:
+ 		//Key diisi sama dengan nama variable yang dipassing
+ 		//Value diisi dengan isi dari variable yang dipassing
+ 		//Untuk cara dibawah, compact('siswa') juga berarti ['siswa' => $siswa ]
+ 		$siswa = DB::table('siswa')->get();
+ 		return View::make('read', compact('siswa'));
+ 		
+ 		//Cara 2, dengan array normal
  		$data = DB::table('siswa')->get();
- 		return View::make('read')->with('siswa',$data);
+ 		return View::make('read', ['siswa' => $data ]);
+ 		
+ 		
+ 		//Jadi ceritanya gini, setelah form tambahdata di-post, masuk ke function tambahdata() di atas.
+ 		//Fungsi tambahdata() melakukan redirect ke url /read bersama ('with') pesan 'message'.
+ 		//Sesuai ketentuan route, url /read memanggil fungsi lihatdata().
+ 		//Di coding yg lama, fungsi lihatdata me-return view dari read bersama ('with') data siswa.
+ 		//Dari cerita di atas, keliatan kalau 'with' dipanggil 2x. Kalau 'with' dipanggil lebih dari 1x 
+ 		//maka data lama yg disimpan 'with' akan dihapus dan diganti dengan data baru, makanya data 'message' selalu hilang
  	} 
 	
 	public function hapusdata($id){
